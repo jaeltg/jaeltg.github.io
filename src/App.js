8f02c80b2e21c68742d596fe25c667bb75abc7d6
jaeltg <jaeltg@hotmail.com>
2021-06-15 13:51:57 +0100

    added components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './components/Home'
+import WorkList from '.components/WorkList'
 
 function App() {
   return (
-    <div className="App">
-    <Home/>
-    </div>
+    <Router>
+      <>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/my-work" component={WorkList}/>
+        </Switch>
+      </>
+    </Router>
   );
 }
 

232fbb2dbd9a674f2d51fd2550d6c806b1d99309
jaeltg <jaeltg@hotmail.com>
2021-06-15 13:27:57 +0100

    created react app

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return(
+        <>
+            <h1>Hello! I'm Jael Tinman, a new software developer!</h1>
+            <button>View My Work</button>
+        </>
+    )
+}
+
+export default Home;
